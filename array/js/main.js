fff3d22469af1d31aec63cee87bd6d4df9cf9613
AytacMensimova22 <aytacmensimova22@gmail.com>
2022-10-23 13:21:38 +0400

    array

---
+++ array/js/main.js
@@ -0,0 +1,20 @@
+
+
+var seyahet = [
+    ["Tebiet",
+        ["Deniz","Hava limani"],"Ismayilli","Almaniya"
+    ],
+    ["Aytac","Gultac","Sema","Nubar","Azima-Esmira"],
+    ["Revan","Sebuhi","Rza","Deyanet"],
+    [
+        "Shah dag",["Yay","Qis"],"Mandarin",
+    ],
+    ["","Baki."]
+];
+
+console.log(seyahet[3][2][0]+seyahet[0][1][0][1]+seyahet[0][1][1][9]+seyahet[2][1][5]+seyahet[0][3][2]+seyahet[0][1][1][4]+seyahet[0][2][3]+
+    seyahet[3][0][5]+seyahet[4][1][3]+seyahet[0][1][1][7]+seyahet[3][0][4]+seyahet[1][4][0]+seyahet[3][1][0][2]+seyahet[1][1][3]+
+    seyahet[3][1][0][1]+seyahet[1][0][4]+seyahet[1][4][5]+"dir")
+
+
+   
